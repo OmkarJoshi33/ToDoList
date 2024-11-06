41a209406f70ce034929c7a6053b4d2611453ff7
omkar <omkarjoshi1231@gmail.com>
2024-11-06 20:52:07 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -78,16 +78,16 @@ function App() {
   };
 
   return (
-    <div className="container py-11 px-20 h-full w-full flex align-middle justify-center aspect-auto">
-      <div className="container  px-10 py-10 aspect-4/6 h-auto w-4/6 bg-slate-100 rounded-3xl shadow-2xl text-wrap">
-        <div className="flex py-11 px-11 align-middle justify-center">
-          <h1 className="text-6xl text-orange-600 font-bold underline decoration-2 underline-offset-4">
+    <div className="2xl:container py-14 px-20 2xl:h-full 2xl:w-full flex align-middle justify-center aspect-auto sm:w-auto sm:h-auto" >
+      <div className="2xl:container  aspect-4/6 2xl:h-full 2xl:w-4/6 bg-slate-100 rounded-3xl shadow-2xl text-wrap sm:h-auto sm:w-auto">
+        <div className="2xl:container flex 2xl:py-10 2xl:px-10 align-middle justify-center sm:px-1 sm:py-1 w-full items-center ">
+          <h1 className="2xl:container 2xl:text-5xl  text-orange-600 font-bold underline decoration-2 underline-offset-4 hover:text-orange-400 hover:animate-bounce duration-75 text-4xl w-full h-auto flex justify-center">
             ToDoList
           </h1>
         </div>
-        <div className="flex align-middle justify-evenly">
+        <div className="2xl:container flex align-middle justify-evenly ">
           <input
-            className="rounded-3xl m-3 p-2 ps-8 w-3/5 text-lg outline-none text-center"
+            className="2xl:container rounded-3xl 2xl:m-3 2xl:p-2 2xl:ps-8 2xl:w-3/5 2xl:text-lg outline-none text-center hover:bg-gray-50 m-3 p-2 ps-8 w-3/5 text-lg "
             onChange={handleChange}
             value={task}
             id="TaskIP"
@@ -97,26 +97,26 @@ function App() {
           <button
             onClick={handleAdd}
             id="btnADD"
-            className="rounded-3xl m-3 w-20 p-2 text-lg text-white font-bold bg-orange-600"
+            className="2xl:container rounded-3xl 2xl:m-3 2xl:w-20 2xl:p-2 2xl:text-lg text-white font-bold bg-orange-600 hover:bg-orange-400  m-3 w-20 p-2 text-lg"
           >
             ADD
           </button>
         </div>
-        <div className="items-center">
+        <div className="2xl:container items-center 2xl:text-lg text-gray-600 2xl:m-7 text-lg mx-6">
           <input
             onChange={toggleFinished}
             type="checkbox"
             checked={showFinished}
-            className="h-4 m-3 w-4 "
+            className="h-4 m-3 w-4  "
           />
           Show Finished
         </div>
-        <div className="todoheading m-4 px-6">
+        <div className=" 2xl:container 2xl:mx-10 2xl:m-4 2xl:px-6 px-12">
           <h1 className="text-xl text-slate-500">Task List</h1>
         </div>
 
         {todo.length === 0 && (
-          <div className="flex m-5 mx-60 my-12 justify-center">
+          <div className="2xl:container flex 2xl:m-5 2xl:mx-60 2xl:my-12 justify-center">
             NO TODOS TO DISPLAY
           </div>
         )}
@@ -125,19 +125,19 @@ function App() {
           //returns the main content
           return (
             (showFinished || !item.isCompleted) && (
-              <div key={item.id} className="container flex max-w-5/6">
-                <div className="container flex max-w-5/6">
-                  <ul className="container flex max-w-5/6">
-                    <li className="flex w-5/6 items-center">
+              <div key={item.id} className="2xl:container flex 2xl:max-w-5/6 2xl:m-1">
+                <div className="2xl:container flex 2xl:max-w-5/6 ">
+                  <ul className="2xl:container flex 2xl:h-full  2xl:max-w-5/6 ">
+                    <li className="2xl:container flex 2xl:w-5/6 items-center 2xl:mx-36 m-1 sm:w-3/4">
                       <input
                         name={item.id}
                         onChange={handleCheckbox}
                         checked={item.isCompleted}
                         type="checkbox"
-                        className="flex h-5 w-5 m-2"
+                        className="flex h-5 w-5"
                       />
-                      <div className="flex items-center  ">
-                        <div className="flex w-96 overflow-y-auto">
+                      <div className="2xl:container flex items-center 2xl:mx-2  2xl:text-lg text-lg">
+                        <div className="2xl:container flex 2xl:w-5/6 overflow-y-auto lg:w-[500px]  xl:w-[800px]  sm:w-[300px] w-[200px]">
                           <span
                             className={item.isCompleted ? "line-through " : ""}
                           >
@@ -145,10 +145,10 @@ function App() {
                           </span>
                         </div>
 
-                        <div className="flex">
+                        <div className="container w-full h-full flex ">
                           <button
                             onClick={() => handleEdit(item.id)}
-                            className="m-1 p-1 text-white h-auto rounded-xl bg-orange-600"
+                            className="m-1 p-1 text-white h-auto rounded-xl bg-orange-600 hover:bg-orange-500"
                             id="Edit"
                             aria-label={`Edit task ${item.task}`}
                           >
@@ -156,7 +156,7 @@ function App() {
                           </button>
                           <button
                             onClick={() => handleDelete(item.id)}
-                            className="m-1 p-2  text-white rounded-xl bg-orange-600"
+                            className="m-1 p-2  text-white rounded-xl bg-orange-600 hover:bg-orange-500"
                             id="Delete"
                             aria-label={`Delete task ${item.task}`}
                           >
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,18 +4,18 @@ function NavBar()
 {
     return(
       <>
-        <nav className="container h-auto w-full bg-black-100 text-white flex justify-around text-lg p-2">
+        <nav className="2xl:container 2xl:h-auto 2xl:w-full  aspect-auto bg-black-100 text-white flex justify-around 2xl:text-lg 2xl:p-2   sm:w-full ">
         <div class="relative group inline-block">
           <span
             className="hover:text-blue-200 "
           >
-            <h1 className="shadow-slate-500 font-bold"> SIMPLY</h1>
+            <h1 className="shadow-slate-500 font-bold hover:animate-bounce"> SIMPLY</h1>
           </span>
          
         </div>
         <div className="flex gap-8">
-          <span className="hover:text-blue-200">Home</span>
-          <span className="hover:text-blue-200">Task</span>
+          <span className="hover:text-blue-200 hover:animate-bounce">Home</span>
+          <span className="hover:text-blue-200 hover:animate-bounce">Task</span>
         </div>
       </nav>
       </>
